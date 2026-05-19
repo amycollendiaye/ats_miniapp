2e6e000da28497a7fc61cd202155d748bdc3b6ce
amycollendiaye <amycollendiaye103@gmail.com>
2026-05-19 19:49:40 +0000

    creation  view

---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+// js
+Page({
+
+  /**
+   * Initial data of the page
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+})
